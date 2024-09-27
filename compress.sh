a028591fda622708772b85fe0ac7c394d6bb2282
Kalon Lucas Kelley <klucas.kelley@gmail.com>
2024-09-27 11:11:16 -0700

    Updated pack format for 24w39a and changed compress script

--- compress.sh
+++ compress.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 DATAPACK_NAME=$(basename ${SCRIPT_DIR})
 rm ${DATAPACK_NAME}.zip
-zip -vr ${DATAPACK_NAME}.zip ./* -x "*.zip" -x "*compress.sh" -x "*~"
+zip -vr ${DATAPACK_NAME}.zip ./* -x "*.zip" -x "*compress.sh" -x "*~" -x "*.git*"
